a961b7e58bef23eab87509a8ffbb666e3d0c0b2a
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2022-09-15 03:42:37 +0900

    Update controller.java

--- src/controller/controller.java
+++ src/controller/controller.java
@@ -1,11 +1,21 @@
-import org.springframework.stereotype.Controller;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
 
 @Controller
-public class controller {
+public class controller implements Controller {
 
   @RequestMapping(value = "/");
-  public String main(String[] args){
+  public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throw Exception {
+    
+    ModelAndView mv = new ModelAndView();
+    mv.addObject("data", "value1");
+    mv.setViewName("post1.jsp");
+    
+    return mv;
   
   }
 }
